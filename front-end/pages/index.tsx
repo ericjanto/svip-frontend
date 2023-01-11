a9a4ba7414cbccb9bcc7056769650184c445e757
Eric Janto <e.janto@gmx.de>
2023-01-11 12:16:23 +0000

    Use same `<Head>` for every page

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -1,14 +1,6 @@
-import Head from 'next/head'
-
 export default function HomePage() {
   return (
     <>
-      <Head>
-        <title>TTDS Search Engine</title>
-        <meta name="description" content="TTDS Search Engine" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        {/* <link rel="icon" href="/favicon.ico" /> */}
-      </Head>
       <main>
         <div>Home page. This will probably only show an input field, not much more.</div>
       </main>
