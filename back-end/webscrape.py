89a1b7bbd6c051ed1c64cfd937d98c0ebcdcf116
Jackw <riotshielder21@gmail.com>
2023-02-14 16:51:55 +0000

    story characters and other info

--- back-end/webscrape.py
+++ back-end/webscrape.py
@@ -1,64 +1,76 @@
 import requests
 from bs4 import BeautifulSoup
 
-# Define the base URL of the Harry Potter fandom page on AO3
-base_url = "https://archiveofourown.org/tags/Harry%20Potter%20-%20J*d*%20K*d*%20Rowling/works"
+# URL for the Harry Potter fandom on Archive of Our Own
+url = 'https://archiveofourown.org/tags/Harry%20Potter%20-%20J*d*%20K*d*%20Rowling/works'
 
-# Send a GET request to the base URL and parse the HTML content with Beautiful Soup
-response = requests.get(base_url)
-soup = BeautifulSoup(response.content, 'html.parser')
+# Retrieve the page HTML using requests
+page = requests.get(url).text
 
-# Find the total number of works in the Harry Potter fandom
-total_works = int(soup.find('h2', class_='heading').find('a').get_text().replace(',', '').split()[-1])
+# Parse the page HTML using BeautifulSoup
+soup = BeautifulSoup(page, 'html.parser')
 
-# Find the number of pages from the navigation bar at the bottom of the page
-navigation = soup.find('ol', class_='pagination actions')
-if navigation is not None:
-    pages = navigation.find_all('li')
-    if len(pages) > 1:
-        total_pages = int(pages[-2].get_text())
-    else:
-        total_pages = 1
-else:
-    total_pages = 1
+# Find the second-to-last item in the list of pages in the navigation bar
+num_pages = int(soup.find(class_='pagination').find_all('li')[-2].get_text())
 
-# Loop through all the pages of works in the Harry Potter fandom on AO3
-for page_num in range(1, total_pages + 1):
-    # Construct the URL of the current page
-    url = base_url + "?page=" + str(page_num)
-    
-    # Send a GET request to the URL
-    response = requests.get(url)
-    
-    # Parse the HTML content of the page with Beautiful Soup
-    soup = BeautifulSoup(response.content, 'html.parser')
-    
-    # Find all the story links on the page
-    story_links = soup.find_all('a', class_='work')
-    
-    # Loop through each story link and retrieve the content, story ID, chapter IDs, and tags
-    for link in story_links:
-        # Get the story ID from the URL
-        story_id = link.get('href').split('/')[-1]
+# Iterate over each page of stories
+for page_num in range(1, num_pages + 1):
 
-        # Send a GET request to the story page
-        story_response = requests.get(link.get('href'))
-        story_soup = BeautifulSoup(story_response.content, 'html.parser')
+    # Construct the URL for the page of stories
+    page_url = url + '?page=' + str(page_num)
 
-        # Find the content of the story
-        story_content = story_soup.find('div', class_='userstuff').get_text()
+    # Retrieve the page HTML using requests
+    page = requests.get(page_url).text
 
-        # Find the chapter IDs of the story
-        chapter_links = story_soup.find_all('li', class_='chapter')
-        chapter_ids = [link.find('a').get('href').split('/')[-1] for link in chapter_links]
+    # Parse the page HTML using BeautifulSoup
+    soup = BeautifulSoup(page, 'html.parser')
 
-        # Find the tags of the story
-        tag_links = story_soup.find_all('li', class_='tag')
-        tags = [link.find('a').get_text() for link in tag_links]
+    # Find the list of stories on the page
+    story_list = soup.find(class_='work index group')
 
-        # Print the story ID, chapter IDs, content, and tags
-        print("Story ID:", story_id)
-        print("Chapter IDs:", chapter_ids)
-        print("Content:", story_content)
-        print("Tags:", tags)
+    # Iterate over each story in the list
+    for story in story_list.find_all('li'):
+
+        # Retrieve the story ID from the story link
+        story_link = story.find(class_='heading').find('a')
+        story_id = story_link['href'].split('/')[-1]
+
+        # Retrieve the content of each chapter in the story
+        content = ''
+        for chapter_link in story.find_all(class_='chapter index group'):
+            chapter_id = chapter_link['id'].split('_')[-1]
+            chapter_url = 'https://archiveofourown.org' + chapter_link.find('a')['href']
+            chapter_page = requests.get(chapter_url).text
+            chapter_soup = BeautifulSoup(chapter_page, 'html.parser')
+            chapter_content = chapter_soup.find(class_='userstuff')
+            if chapter_content:
+                content += chapter_content.get_text().strip() + '\n'
+
+        # Retrieve the tags for the story
+        tag_list = story.find(class_='tags commas').find_all('li')
+        characters = []
+        freeforms = []
+        warnings = []
+        relationships = []
+        for tag in tag_list:
+            tag_type = tag['class'][0]
+            tag_text = tag.get_text().strip()
+            if tag_type == 'character':
+                characters.append(tag_text)
+            elif tag_type == 'freeform':
+                freeforms.append(tag_text)
+            elif tag_type == 'warning':
+                warnings.append(tag_text)
+            elif tag_type == 'relationship':
+                relationships.append(tag_text)
+
+        # Print the results for the story
+        print('Story ID:', story_id)
+        print('Chapter IDs:', [c['id'].split('_')[-1] for c in story.find_all(class_='chapter index group')])
+        print('Content:', content)
+        print('Characters:', characters)
+        print('Freeforms:', freeforms)
+        print('Warnings:', warnings)
+        print('Relationships:', relationships)
+        print('===============================')
 
