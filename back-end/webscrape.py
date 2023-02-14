c0071bef8528b004322f95fd5c8310adc160a886
Jackw <riotshielder21@gmail.com>
2023-02-14 16:26:16 +0000

    story ide etc

--- back-end/webscrape.py
+++ back-end/webscrape.py
@@ -13,6 +13,23 @@ soup = BeautifulSoup(response.content, 'html.parser')
 # Find all the story links on the page
 story_links = soup.find_all('a', class_='work')
 
-# Print the titles of each story
+# Loop through each story link and retrieve the content, story ID, and chapter IDs
 for link in story_links:
-    print(link.text)
+    # Get the story ID from the URL
+    story_id = link.get('href').split('/')[-1]
+    
+    # Send a GET request to the story page
+    story_response = requests.get(link.get('href'))
+    story_soup = BeautifulSoup(story_response.content, 'html.parser')
+    
+    # Find the content of the story
+    story_content = story_soup.find('div', class_='userstuff').get_text()
+    
+    # Find the chapter IDs of the story
+    chapter_links = story_soup.find_all('li', class_='chapter')
+    chapter_ids = [link.find('a').get('href').split('/')[-1] for link in chapter_links]
+    
+    # Print the story ID, chapter IDs, and content
+    print("Story ID:", story_id)
+    print("Chapter IDs:", chapter_ids)
+    print("Content:", story_content)
