e8fadcffbc58d3cd2019de254e5243e9554f726d
Eric Janto <e.janto@gmx.de>
2023-03-10 15:35:14 +0000

    Improve advanced query detection

--- front-end/components/FeatureDetector.tsx
+++ front-end/components/FeatureDetector.tsx
@@ -9,13 +9,13 @@ export default function FeatureDetector({ searchQuery }: FeatureDetectorProps) {
     const { query, tags, wildcardUse, booleanUse } = destructQuery(searchQuery)
 
     return (
-            <details className="text-sm text-gray-500">
-                <summary>Advanced query explainer (wip)</summary>
-                <ul className="list-disc ml-8">
-                    <li>Tags use: <code>{String(tags != null)}</code></li>
-                    <li>Wildcard use: <code>{String(wildcardUse)}</code></li>
-                    <li>Boolean use: <code>{String(booleanUse)}</code></li>
-                </ul>
-            </details>
+        <details className="text-sm text-gray-500">
+            <summary>Advanced query explainer (wip)</summary>
+            <ul className="list-disc ml-8">
+                <li>Tags use: <code>{String(tags != null)}</code></li>
+                <li>Wildcard use: <code>{String(wildcardUse)}</code></li>
+                <li>Boolean use: <code>{String(booleanUse)}</code></li>
+            </ul>
+        </details>
     )
 }
--- front-end/utils.tsx
+++ front-end/utils.tsx
@@ -7,8 +7,18 @@ type DestructedQuery = {
 
 export function destructQuery(searchQuery: string): DestructedQuery {
     const query = searchQuery
-    const tags = searchQuery.match(/#\w+/g)
-    const wildcardUse = new RegExp(/\*\S|\S\*|\S\*\S/g).test(searchQuery)
-    const booleanUse = new RegExp(/AND +.|OR +./g).test(searchQuery)
+    const tags = searchQuery.match(/\s#\S+/g)
+    const booleanUse = new RegExp(/\S+ AND +\S|\S+ OR +\S/g).test(searchQuery)
+
+    const potentialWildcards = searchQuery.match(/\*\S+|\S+\*|\S+\*\S+/g)
+
+    var wildcardUse
+    // Check that none of them are tags
+    if (!potentialWildcards) {
+        wildcardUse = false
+    } else {
+        wildcardUse = potentialWildcards!.every((e) => e.at(0) != '#')
+    }
+
     return { query, tags, wildcardUse, booleanUse }
 }
