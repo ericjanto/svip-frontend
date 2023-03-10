3291cda47cfdba9900e5562f3411736b8635eafe
Eric Janto <e.janto@gmx.de>
2023-03-10 16:29:17 +0000

    Improve feature detector

--- front-end/components/FeatureDetector.tsx
+++ front-end/components/FeatureDetector.tsx
@@ -6,15 +6,50 @@ type FeatureDetectorProps = {
 }
 
 export default function FeatureDetector({ searchQuery }: FeatureDetectorProps) {
-    const { query, tags, wildcardUse, booleanUse } = destructQuery(searchQuery)
+    const { query, tags, wildcardUse, booleanUse, exactSearchUse } = destructQuery(searchQuery)
+
+    const tagUse = tags != null
+    var usedFeatures = 0
+    if (tagUse) usedFeatures++
+    if (wildcardUse) usedFeatures++
+    if (booleanUse) usedFeatures++
+    if (exactSearchUse) usedFeatures++
+    
 
     return (
         <details className="text-sm text-gray-500">
-            <summary>Advanced query explainer (wip)</summary>
+            <summary>Tip: Use advanced query syntax to refine your search ({usedFeatures}/4)</summary>
             <ul className="list-disc ml-8">
-                <li>Tags use: <code>{String(tags != null)}</code></li>
-                <li>Wildcard use: <code>{String(wildcardUse)}</code></li>
-                <li>Boolean use: <code>{String(booleanUse)}</code></li>
+                <li className={tagUse ? "text-green-600" : ""}>
+                    Filter results with story tags:
+                    <code>
+                        <span className="text-blue-500"> #</span>
+                        &lt;tag&gt;
+                    </code>
+                </li>
+                <li className={wildcardUse ? "text-green-600" : ""}>Use wildcard search:{' '}
+                    <code>
+                        direction of{' '}
+                        <span className="text-blue-500">*</span>
+                        east
+                    </code>
+                </li>
+                <li className={booleanUse ? "text-green-600" : ""}>Use boolean search:{' '}
+                    <code>
+                        term1
+                        <span className="text-blue-500"> AND </span>
+                        term2
+                        <span className="text-blue-500"> OR </span>
+                        term3
+                    </code>
+                </li>
+                <li className={exactSearchUse ? "text-green-600" : ""}>Use exact phrasal search:{' '}
+                    <code>
+                        <span className="text-blue-500">"</span>
+                        term1 term2
+                        <span className="text-blue-500">"</span>
+                    </code>
+                </li>
             </ul>
         </details>
     )
--- front-end/utils.tsx
+++ front-end/utils.tsx
@@ -3,22 +3,26 @@ type DestructedQuery = {
     tags: string[] | null,
     wildcardUse: boolean,
     booleanUse: boolean,
+    exactSearchUse: boolean
 }
 
 export function destructQuery(searchQuery: string): DestructedQuery {
     const query = searchQuery
-    const tags = searchQuery.match(/\s#\S+/g)
-    const booleanUse = new RegExp(/\S+ AND +\S|\S+ OR +\S/g).test(searchQuery)
+    const tags = searchQuery.match(/\s#\S+|^#\S+/g)
 
-    const potentialWildcards = searchQuery.match(/\*\S+|\S+\*|\S+\*\S+/g)
+    const cleanQuery = searchQuery.replaceAll(/\s#\S+|^#\S+/g, '')
+    const booleanUse = new RegExp(/\S+ AND +\S|\S+ OR +\S/g).test(cleanQuery)
 
+    const potentialWildcards = cleanQuery.match(/\*\S+|\S+\*\S+|\S+\*|\w+\s+\*\s+\w+/g)
     var wildcardUse
     // Check that none of them are tags
     if (!potentialWildcards) {
         wildcardUse = false
     } else {
-        wildcardUse = potentialWildcards!.every((e) => e.at(0) != '#')
+        wildcardUse = !potentialWildcards!.every((e) => e.at(0) == '#')
     }
 
-    return { query, tags, wildcardUse, booleanUse }
+    const exactSearchUse = new RegExp(/\".+\"/g).test(cleanQuery)
+
+    return { query, tags, wildcardUse, booleanUse, exactSearchUse }
 }
