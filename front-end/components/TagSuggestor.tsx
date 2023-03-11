992ae8a39520cda2cfbf09107d5b323f23f1e9b6
Eric Janto <e.janto@gmx.de>
2023-03-11 18:22:33 +0000

    Use mock api for tag autocomplete

--- front-end/components/TagSuggestor.tsx
+++ front-end/components/TagSuggestor.tsx
@@ -1,15 +1,33 @@
 import { Dispatch, RefObject, SetStateAction } from "react"
+import useSWR, { Fetcher } from "swr"
 
 type TagSuggestorProps = {
     currentEditedTag: string,
     currentQuery: string,
-    setCurrentQuery:  Dispatch<SetStateAction<string>>,
+    setCurrentQuery: Dispatch<SetStateAction<string>>,
     setCurrentlyEditedTag: Dispatch<SetStateAction<string>>,
     queryInputRef: RefObject<HTMLInputElement>,
 }
 
+type TagApiResults = {
+    id: Number,
+    tag: string,
+}[]
+
+const fetcher: Fetcher<TagApiResults> = (url: RequestInfo | URL) => fetch(url).then(r => r.json())
+const API_URL = 'https://63be76d1e348cb07620f5001.mockapi.io/api/mock/tags'
+
 export default function TagSuggestor({ currentEditedTag, setCurrentlyEditedTag, currentQuery, setCurrentQuery, queryInputRef }: TagSuggestorProps) {
     // Here, make request to tag autocomplete API
+    const { data, error, isLoading } = useSWR(API_URL, fetcher)
+
+    const topFiveTags: string[] = []
+    if (data) {
+        data.slice(0, 5).forEach((item) => {
+            topFiveTags.push(item.tag)
+        })
+    }
+
     const dummyTagSuggestions = ['tag1', 'tag2', 'tag3', 'tag4', 'tag4']
 
     const handleClick = (e: any) => {
@@ -27,30 +45,35 @@ export default function TagSuggestor({ currentEditedTag, setCurrentlyEditedTag, 
 
     return (
         <div className="mb-[-22px] mt-4">
-            {dummyTagSuggestions.map(
-                (tag) =>
-            <button
-                className="
-                relative
-                text-xs
-                p-1
-                pl-2
-                pr-2
-                bg-gray-100
-                rounded
-                transition
-                ease-in-out
-                mr-2
-                focus:bg-gray-200
-                hover:bg-gray-200
-                z-[9999999]
-                "
-                onClick={handleClick}
-                key={tag}
-            >
-                {tag}
-            </button>
-            )}
+            {isLoading
+                ? <div className="text-sm">Loading tag suggestions...</div>
+                : !data
+                    ? <div className="text-sm">There are no similar tags!</div>
+                    : topFiveTags.map(
+                        (tag) =>
+                            <button
+                                className="
+                        relative
+                        text-xs
+                        p-1
+                        pl-2
+                        pr-2
+                        bg-gray-100
+                        rounded
+                        transition
+                        ease-in-out
+                        mr-2
+                        focus:bg-gray-200
+                        hover:bg-gray-200
+                        z-[9999999]
+                        "
+                                onClick={handleClick}
+                                key={tag}
+                            >
+                                {tag.toLowerCase()}
+                            </button>
+                    )
+            }
         </div>
     )
 }
