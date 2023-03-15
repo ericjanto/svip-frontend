bfd1e8c76824dffea89f4b0be2dd3fb29afc0257
Eric Janto <e.janto@gmx.de>
2023-03-15 23:24:03 +0000

    Add title for front page

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -4,7 +4,7 @@ export default function HomePage() {
   return (
     <>
       <main>
-        <h1 className="container mx-auto text-center text-lg mt-20">Logo Placeholder</h1>
+        <h1 className="container mx-auto text-center text-lg mt-20">AO3 Search Engine</h1>
         <div className='container px-24 space-y-3 max-w-4xl mx-auto'>
           <QueryInput initialState='' />
         </div>
