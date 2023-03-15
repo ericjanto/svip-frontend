ae0c218affc40ac7e0333c00c7a29af58bdf6566
Eric Janto <e.janto@gmx.de>
2023-03-15 23:24:27 +0000

    Rename tab title

--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -6,8 +6,8 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>TTDS Search Engine</title>
-        <meta name="description" content="TTDS Search Engine" />
+        <title>AO3 Search Engine</title>
+        <meta name="description" content="AO3 Search Engine" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         {/* <link rel="icon" href="/favicon.ico" /> */}
       </Head>
